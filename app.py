e7b71b47542759f4888d5bcf6db2728f22ee6a28
Luis Ríos <141874238+LuisHRiosC18@users.noreply.github.com>
2025-08-13 11:42:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def generate_report(cartera, excel_1, excel_2, proyecciones, ecobro_reporte):
 
     # 4. Calcular el Resultado final y las columnas de aportación
     def calcular_resultados_finales(row):
-        lista_de_prioridad = ['Cobro', 'No tenía dinero', 'Difirió el pago']
+        lista_de_prioridad = ['Cobro', 'Difirió el pago', 'No tenía dinero' ]
         resultado_final = ''
         ultimo_detalle_encontrado = ''
         
